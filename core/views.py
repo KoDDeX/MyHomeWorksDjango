2a301e3a1ff1c1db7b433312bfd00769d6670352
Ivan Bunko <kodexx@bk.ru>
2025-04-16 23:40:51 +0300

    HW_39: Занес в базу 5 видов услуг

--- core/views.py
+++ core/views.py
@@ -38,3 +38,55 @@ def order_detail(request, order_id):
         'menu_items': MENU_ITEMS,
     }
     return render(request, 'core/order_detail.html', context)
+
+
+# order = Order.objects.create(
+#     client_name="Иван Иванов",
+#     phone="+79991234567",
+#     comment="Нужно сделать стрижку",
+#     status="not_approved",
+#     appointment_date='2025-10-01 10:00:00',
+#     master=Master.objects.get(id=1),
+#     services=Service.objects.get(id=1),
+# )
+
+# Service.objects.create(
+#     name="Стрижка",
+#     price=1000,
+#     duration=60,
+#     description="Стрижка мужского волоса",
+# )
+# Service.objects.create(
+#     name="Стрижка + борода",
+#     price=1500,
+#     duration=90,
+#     description="Стрижка мужского волоса и бороды",
+# )
+
+# Service.objects.create(
+#     name="Покраска бороды",
+#     price=2000,
+#     duration=120,
+#     description="Покраска бороды и усов из мужского волоса",
+# )
+
+# Service.objects.create(
+#     name="Покраска волос",
+#     price=2500,
+#     duration=150,
+#     description="Покраска волос мужского волоса",
+# )
+
+# Service.objects.create(
+#     name="Покраска волос + борода",
+#     price=3000,
+#     duration=180,
+#     description="Покраска волос и бороды из мужского волоса",
+# )
+
+# Service.objects.create(
+#     name="Стрижка Сахара",
+#     price=3500,
+#     duration=210,
+#     description="Стрижка наголо",
+# )
