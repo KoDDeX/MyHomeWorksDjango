342464ee052fe7bc10e08cd48edbdd05ab10cdfc
Ivan Bunko <kodexx@bk.ru>
2025-05-13 00:12:20 +0300

    HW_42: Настроил административную панель для работы с отзывами

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,25 @@ admin.site.register(Order)
 admin.site.register(Service)
 admin.site.register(Master)
 
+# class ReviewAdmin(admin.ModelAdmin):
+#     list_filter = ('client_name', 'master', 'created_at')
+# admin.site.register(Review, ReviewAdmin)
+
+@admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_filter = ('client_name', 'master', 'created_at')
-admin.site.register(Review, ReviewAdmin)
+    list_display = ('client_name', 'master', 'rating', 'created_at', 'is_published')
+    list_filter = ('is_published', 'rating', 'master')
+    search_fields = ('client_name', 'text')
+    list_editable = ('is_published',)
+    readonly_fields = ('created_at',)
+    
+    # Дополнительные действия
+    actions = ['publish_reviews', 'unpublish_reviews']
+    
+    def publish_reviews(self, request, queryset):
+        queryset.update(is_published=True)
+    publish_reviews.short_description = "Опубликовать выбранные отзывы"
+    
+    def unpublish_reviews(self, request, queryset):
+        queryset.update(is_published=False)
+    unpublish_reviews.short_description = "Снять с публикации выбранные отзывы"
